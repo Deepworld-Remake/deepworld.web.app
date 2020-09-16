e202767c912f2e8f9b9a07953182f6910006dee4
Sir Code <taylorschneider07@yahoo.com>
2020-09-16 07:22:01 -0400

    Updated Items page

---
+++ public/Javascript/items.js
@@ -0,0 +1,34 @@
+var itemData;
+
+function findWithAttr(array, attr, value) {
+    for(var i = 0; i < array.length; i += 1) {
+        if(array[i][attr] === value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        var obj = this.responseText;
+        itemData = JSON.parse(obj);
+        const urlParams = new URLSearchParams(window.location.search);
+        if (!urlParams.has("itemId")) {
+            window.location.replace("http://deepworld.web.app/codex");
+        }
+        const currentItem = findWithAttr(itemData, "id", urlParams.get("itemId"));
+        if (currentItem.name) {
+            var anchor = document.createElement("a");
+            anchor.setAttribute("class", "itemDataLink");
+            anchor.setAttribute("href", `./items?itemId=${currentItem.id}`);
+            anchor.innerHTML = `<img class="itemDataIcon" src="https://dwre-codex.web.app/Sprites/${currentItem.sprite}.png"><span class="itemDataText">${currentItem.ui}</span><div class="itemDataId">${currentItem.id}</div>`;
+            document.querySelector(".itemData").appendChild(anchor);
+        }
+    }
+};
+
+xhttp.open("GET", "https://raw.githubusercontent.com/Deepworld-Remake/Deepworld-Codex-Remastered/master/ItemConfig.json", true);
+xhttp.send();
