4712154d6255311f073f74adcc36fe14938e58ca
Sir Code <44554976+MisterSirCode@users.noreply.github.com>
2021-04-30 07:05:14 -0400

    Update downloads.php

--- html/Pages/downloads.php
+++ html/Pages/downloads.php
@@ -39,7 +39,9 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
 		integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 	<style>
-
+		h4 {
+			color: white;
+		}
 	</style>
 	<link rel="stylesheet" href="../Stylesheets/index.css">
 </head>
