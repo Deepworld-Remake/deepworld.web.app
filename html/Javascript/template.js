2f5c5dc61e71583d22e74dca8dd330f2df2dcbf4
Sir Code <taylorschneider07@yahoo.com>
2021-05-17 19:50:03 -0700

    Update the templater

--- html/Javascript/template.js
+++ html/Javascript/template.js
@@ -9,7 +9,6 @@ const webLinks = {
 function loadWebLinks(element, socket, currentPage) {
     for (var i = 0; i < Object.keys(webLinks); ++i) {
         const currentWebLink = webLinks[Object.keys(webLinks)[i]];
-        const storageElement = document.querySelector(element);
         const listElement = document.createElement("li");
         listElement.classList.add("nav-item");
         const linkElement = document.createElement("a");
@@ -18,10 +17,10 @@ function loadWebLinks(element, socket, currentPage) {
             linkElement.href = socket + currentWebLink[0];
         linkElement.innerText = currentWebLink[1];
         listElement.appendChild(linkElement);
-        storageElement.appendChild(listElement);
+        document.querySelector(element).appendChild(listElement);
     }
 }
 
 function loadArtContent(element) {
-    
+
 }
