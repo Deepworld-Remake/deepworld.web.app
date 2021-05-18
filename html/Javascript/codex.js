c7d97efa2f5074773b0065ab76be598de62f7429
Sir Code <taylorschneider07@yahoo.com>
2021-05-18 16:29:55 -0700

    Remove my dumdum oopsie

--- html/Javascript/codex.js
+++ html/Javascript/codex.js
@@ -4,7 +4,6 @@ xhr.addEventListener("load", (e) => {
     const text = e.responseText;
     console.log(JSON.parse(text));
 });
-const text = e.responseText;
 xhr.overrideMimeType("text/plain");
 xhr.open("GET", "https://cdn.jsdelivr.net/gh/Deepworld-Remake/deepworld-config-json@master/config-items.json");
 xhr.send();
