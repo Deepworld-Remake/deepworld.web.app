4322c519d917216ac9f05c817c3d5db23bd2f425
Sir Code <taylorschneider07@yahoo.com>
2021-05-18 16:34:25 -0700

    OK nvm that sucked lemme try this..

--- html/Javascript/codex.js
+++ html/Javascript/codex.js
@@ -1,6 +1,9 @@
 const xhr = new XMLHttpRequest();
 xhr.addEventListener("load", (e) => {
     console.log(e);
+    const text = e.target.responseText;
+    console.log(JSON.parse(text));
 });
+xhr.overrideMimeType("text/plain");
 xhr.open("GET", "https://cdn.jsdelivr.net/gh/Deepworld-Remake/deepworld-config-json@master/config-items.json");
 xhr.send();
