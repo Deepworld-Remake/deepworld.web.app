49480bbf2ecb450d566ffbf84ae3aad5413e60fd
Sir Code <taylorschneider07@yahoo.com>
2021-05-18 16:28:52 -0700

    Update XHR Request for Codex API

--- html/Javascript/codex.js
+++ html/Javascript/codex.js
@@ -4,5 +4,7 @@ xhr.addEventListener("load", (e) => {
     const text = e.responseText;
     console.log(JSON.parse(text));
 });
+const text = e.responseText;
+xhr.overrideMimeType("text/plain");
 xhr.open("GET", "https://cdn.jsdelivr.net/gh/Deepworld-Remake/deepworld-config-json@master/config-items.json");
 xhr.send();
