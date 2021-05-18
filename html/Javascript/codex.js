02cbabd42af442faa5909345d923cbda39a9f5a5
Sir Code <taylorschneider07@yahoo.com>
2021-05-18 16:32:26 -0700

    Legit JSON Parsing...

--- html/Javascript/codex.js
+++ html/Javascript/codex.js
@@ -1,9 +1,6 @@
 const xhr = new XMLHttpRequest();
 xhr.addEventListener("load", (e) => {
-    console.log(e.responseText);
-    const text = e.responseText;
-    console.log(JSON.parse(text));
+    console.log(e);
 });
-xhr.overrideMimeType("text/plain");
 xhr.open("GET", "https://cdn.jsdelivr.net/gh/Deepworld-Remake/deepworld-config-json@master/config-items.json");
 xhr.send();
