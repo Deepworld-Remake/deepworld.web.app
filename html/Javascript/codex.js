c727a7cdb94ae57b3c9cfd506b3be3327ab340ad
Sir Code <taylorschneider07@yahoo.com>
2021-06-12 09:44:50 -0400

    Update codex.js

--- html/Javascript/codex.js
+++ html/Javascript/codex.js
@@ -20,40 +20,49 @@ function imageExists(url) {
 
 function loadItemData() {
     const xhr = new XMLHttpRequest();
-    xhr.addEventListener("load", (e) => {
-        console.log(e);
-        const text = e.target.responseText;
-        const json = JSON.parse(text);
-        itemDataCache = json;
-        const rowElm = document.createElement("div");
-        rowElm.classList.add("col-lg-4", "col-md-4", "col-sm-12");
-        const titleElm = document.createElement("h1");
-        titleElm.classList.add("feature-title");
-        titleElm.innerText = "Items and Collectibles";
-        rowElm.appendChild(titleElm);
-        for (let i = 0; i < Object.keys(json.items).length; ++i) {
-            const currentKey = Object.keys(json.items)[i];
-            const currentItem = json.items[currentKey];
-            if (currentItem.code && (currentItem.gui ? currentItem.gui : true)) {
-                const contentElm = document.createElement("p");
-                const contentImg = document.createElement("object");
-                const contentNoneImg = document.createElement("img");
-                const contentTitle = document.createElement("a");
-                contentImg.classList.add("item-object");
-                contentNoneImg.classList.add("item-image");
-                contentImg.data = spriteURL + (currentItem.inventory ? currentItem.inventory : currentKey) + ".png";
-                contentNoneImg.src = spriteURL + "none.png";
-                contentImg.appendChild(contentNoneImg);
-                contentElm.appendChild(contentImg);
-                contentTitle.innerText += currentKey;
-                contentTitle.classList.add("item-title");
-                contentElm.appendChild(contentTitle);
-                rowElm.appendChild(contentElm);
+    xhr.addEventListener("load", (ec) => {
+        const xhr2 = new XMLHttpRequest();
+        const textConfig = e.target.responseText;
+        const jsonConfig = JSON.parse(textConfig);
+        console.log(jsonConfig.inventory);
+        xhr2.addEventListener("load", (e) => {
+            console.log(e);
+            const text = e.target.responseText;
+            const json = JSON.parse(text);
+            itemDataCache = json;
+            const rowElm = document.createElement("div");
+            rowElm.classList.add("col-lg-4", "col-md-4", "col-sm-12");
+            const titleElm = document.createElement("h1");
+            titleElm.classList.add("feature-title");
+            titleElm.innerText = "Items and Collectibles";
+            rowElm.appendChild(titleElm);
+            for (let i = 0; i < Object.keys(json.items).length; ++i) {
+                const currentKey = Object.keys(json.items)[i];
+                const currentItem = json.items[currentKey];
+                if (currentItem.code && (currentItem.gui ? currentItem.gui : true)) {
+                    const contentElm = document.createElement("p");
+                    const contentImg = document.createElement("object");
+                    const contentNoneImg = document.createElement("img");
+                    const contentTitle = document.createElement("a");
+                    contentImg.classList.add("item-object");
+                    contentNoneImg.classList.add("item-image");
+                    contentImg.data = spriteURL + (currentItem.inventory ? currentItem.inventory : currentKey) + ".png";
+                    contentNoneImg.src = spriteURL + "none.png";
+                    contentImg.appendChild(contentNoneImg);
+                    contentElm.appendChild(contentImg);
+                    contentTitle.innerText += currentKey;
+                    contentTitle.classList.add("item-title");
+                    contentElm.appendChild(contentTitle);
+                    rowElm.appendChild(contentElm);
+                }
             }
-        }
-        document.querySelector(".row").appendChild(rowElm);
+            document.querySelector(".row").appendChild(rowElm);
+        });
+        xhr2.overrideMimeType("text/plain");
+        xhr2.open("GET", "https://cdn.jsdelivr.net/gh/Deepworld-Remake/deepworld-config-json@master/config-items.json");
+        xhr2.send();
     });
     xhr.overrideMimeType("text/plain");
-    xhr.open("GET", "https://cdn.jsdelivr.net/gh/Deepworld-Remake/deepworld-config-json@master/config-items.json");
+    xhr.open("GET", "https://cdn.jsdelivr.net/gh/Deepworld-Remake/deepworld-config-json@master/config.json");
     xhr.send();
 }
